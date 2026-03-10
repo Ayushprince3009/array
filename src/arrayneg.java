fed563926ec7e849263a017a0e79344fef490d10
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-11 03:01:21 +0530

    negative array picked

---
+++ src/arrayneg.java
@@ -0,0 +1,11 @@
+public class arrayneg {
+    //print negative elements of an array
+    public static void main(String[] args) {
+        int[] arr = {1,3,6,-1,33,-78,9,-123,-1,56,-99,91};
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]<0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
