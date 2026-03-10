b2251f9a72e7a30e3408e230667d38eef555b4da
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-11 02:47:19 +0530

    array doubled

---
+++ src/arrayDouble.java
@@ -0,0 +1,15 @@
+public class arrayDouble {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = 2*arr[i];
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+    }
+}
