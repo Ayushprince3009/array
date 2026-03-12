dbcc7ed2b6e515dec6ab89301966d9ba669a466f
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-13 01:47:53 +0530

    new questions

---
+++ src/arrayTran.java
@@ -0,0 +1,9 @@
+public class arrayTran {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4,5,6,7};
+         for(int ele : arr){
+             System.out.print(2*ele+" ");
+         }
+
+    }
+}
