5f8100c056d255872bd8c75e7d6b1fba78504b94
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-13 01:58:16 +0530

    new questions

---
+++ src/reverseArray.java
@@ -0,0 +1,19 @@
+public class reverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int i=3;
+        int j = 5;   //yaha i aur j ka value dene se specific range ka araay reverse hoga baki saME RHEGA
+
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+        for(int ele : arr){
+            System.out.print(ele +" ");
+        }
+    }
+}
