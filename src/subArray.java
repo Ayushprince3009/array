8c27a4a20de79e6da133281b596d6dc877bcc6f3
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-30 13:52:09 +0530

    sub array

---
+++ src/subArray.java
@@ -0,0 +1,15 @@
+public class subArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=i; j<arr.length; j++){
+                for(int k=i; k<=j; k++){
+                    System.out.print(arr[k]+ " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
